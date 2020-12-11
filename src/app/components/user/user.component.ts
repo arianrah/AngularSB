583ccbac6d5b119d3f61f504287b2f2271ca8c79
arianrah <arianrahimi@icloud.com>
2020-12-11 08:56:06 -0800

    ng g c component/users

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
-import { userInfo } from 'os';
+
+import { User } from '../../models/User'
 
 @Component({
   selector: 'app-user',
@@ -9,4 +10,20 @@ import { userInfo } from 'os';
 
 export class UserComponent {
 
-}
+  //properties
+  user: User;
+
+  //methods
+  constructor() {
+    this.user = {
+      firstName: 'Ari',
+      lastName: 'Rah',
+      age: 25,
+      address: {
+        street: '41st Avenue',
+        city: 'Vancouver',
+        province: 'BC'
+      }
+    }
+  };
+};
