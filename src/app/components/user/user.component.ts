c8a64d151862282d8047f306f434ea464208552e
arianrah <arianrahimi@icloud.com>
2020-12-11 08:20:20 -0800

    first component

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+import { userInfo } from 'os';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+
+export class UserComponent {
+
+}
