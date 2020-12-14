cb5863f1faf723264fd5ec971b08d6121948e49e
arianrah <arianrahimi@icloud.com>
2020-12-14 09:42:04 -0800

    Template fun. users.component

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../models/User'
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+      this.users = [
+        {
+          firstName: 'Ari',
+          lastName: 'Rah',
+          age: 25,
+          address: {
+            street: '41st Avenue',
+            city: 'Vancouver',
+            province: 'BC'
+          }
+        },
+        {
+          firstName: 'Bob',
+          lastName: 'The Builder',
+          age: 32,
+          address: {
+            street: '52nd Avenue',
+            city: 'West Vancouver',
+            province: 'BC'
+          }
+        },
+        {
+          firstName: 'Steven',
+          lastName: 'Aoki',
+          age: 29,
+          address: {
+            street: 'Fraser St',
+            city: 'Vancouver',
+            province: 'BC'
+          }
+        }
+      ];
+      this.loaded = true;
+    // this.showExtended = false;
+    this.addUser({
+        firstName: 'Michael',
+        lastName: 'Angelo'
+    });
   }
-
+  addUser(user: User) {
+    this.users.push(user)
+  }
 }
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,10 +1,10 @@
 export interface User {
   firstName: string,
   lastName: string,
-  age: number,
-  address: {
-    street: string,
-    city: string,
-    province: string
+  age?: number,
+  address?: {
+    street?: string,
+    city?: string,
+    province?: string
   }
 }
