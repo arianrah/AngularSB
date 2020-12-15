28b30802773552d0805377c79f7130f968c06fc0
arianrah <arianrahimi@icloud.com>
2020-12-15 14:44:08 -0800

    post-form post request to db

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -4,6 +4,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http'
 
 import { Post } from '../models/Post'
 
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,5 +17,9 @@ export class PostService {
 
   getPosts(): Observable<Post[]> {
     return this.http.get<Post[]>(this.postsUrl);
+  }
+
+  savePost(post: Post): Observable<Post> {
+    return this.http.post<Post>(this.postsUrl, post, httpOptions)
   }
 }
