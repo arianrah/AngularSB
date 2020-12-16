cd84afae887ae63434783744190a3f2fe7f00c4e
arianrah <arianrahimi@icloud.com>
2020-12-15 17:08:32 -0800

    Update post functionality started, PUT req next.

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -10,6 +10,12 @@ import { Post } from '../../models/Post'
 })
 export class PostsComponent implements OnInit {
   posts: Post[];
+  currentPost: Post = {
+    id: 0,
+    title: '',
+    body: ''
+  }
+  isEdit: boolean = false;
 
   constructor(private postService: PostService) { }
 
@@ -21,6 +27,12 @@ export class PostsComponent implements OnInit {
 
   onNewPost(post: Post) {
     this.posts.unshift(post);
+  }
+
+  editPost(post: Post) {
+    // console.log('Edit post hit', post)
+    this.currentPost = post;
+    this.isEdit = true;
   }
 
 }
